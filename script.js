d7e238a4228acbae1eec27e1e3c84144a6c70498
happyjoel <joel.stromgren@gmail.com>
2024-02-27 17:07:43 +0100

    Javascript fortsättning

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
+
 // GENERAL SELECTORS
 const planets = document.querySelectorAll(".planet");
-const sun = document.querySelector("planet-sun");
+const sun = document.querySelector(".planet-sun");
 const modal = document.querySelector(".modal");
 const blurOverlay = document.querySelector(".blur-overlay");
 const searchInput = document.querySelector(".search-input");
@@ -28,39 +29,86 @@ planets.forEach((planet) => {
   planet.addEventListener("click", (e) => {
     e.preventDefault();
     console.log(e.target);
-    toggleBlur();
-    toggleModal();
-    toggleInputfield();
+    getPlanetData(e.target.dataset.index);
   });
+});
+
+sun.addEventListener("click", (e) => {
+  e.preventDefault();
+  getPlanetData(e.target.dataset.index);
+  console.log(e.target);
 });
 
 // Toggle MODAL och BLUR när man klickar blurOverlay
 blurOverlay.addEventListener("click", (e) => {
   e.preventDefault();
-  toggleBlur();
   toggleModal();
-  toggleInputfield();
 });
 
 // Toggle MODAL och BLUR om man trycker ESC och om modal innehåller classen inactive
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape" && !modal.classList.contains("inactive")) {
-    toggleBlur();
     toggleModal();
-    toggleInputfield();
   }
 });
+
+// searchInput.addEventListener('submit', (e) => {
+// getPlanetData
+// })
 
 // --------    FUNCTIONS
 
 const toggleModal = function () {
   modal.classList.toggle("inactive");
-};
-
-const toggleBlur = function () {
   blurOverlay.classList.toggle("inactive");
+  searchInput.classList.toggle("inactive-opacity");
 };
 
-const toggleInputfield = function () {
-  searchInput.classList.toggle("inactive-opacity");
+// API Info
+
+const getPlanetData = function (planetID) {
+  fetch("https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies", {
+    method: "GET",
+    headers: { "x-zocom": "solaris-2ngXkR6S02ijFrTP" },
+  })
+    .then((response) => response.json())
+    .then((data) => renderPlanetDescription(data.bodies[planetID]))
+    .catch((error) => console.log(error));
+};
+
+const renderPlanetDescription = function (data) {
+  console.log(data);
+  const html = `<header class="modal-header">
+  <h1 class="modal-heading">${data.name}</h1>
+  <h2 class="modal-subheading">${data.latinName}</h2>
+</header>
+
+<p class="modal-description">
+ ${data.desc}
+</p>
+
+<div class="modal-fact-container">
+  <div>
+    <p class="fact-heading" id="fact-heading-1">OMKRETS</p>
+    <p class="fact" id="fact-1">${data.circumference}</p>
+  </div>
+
+  <div>
+    <p class="fact-heading" id="fact-heading-2">KM FRÅN SOLEN</p>
+    <p class="fact" id="fact-2">${data.distance}</p>
+  </div>
+
+  <div>
+    <p class="fact-heading" id="fact-heading-3">MAX TEMPERATUR</p>
+    <p class="fact" id="fact-3">${data.temp.day}</p>
+  </div>
+
+  <div>
+    <p class="fact-heading" id="fact-heading-4">MIN TEMPERATUR</p>
+    <p class="fact" id="fact-4">${data.temp.night}</p>
+  </div>
+</div>`;
+  modal.innerHTML = "";
+  modal.insertAdjacentHTML("afterbegin", html);
+  toggleModal();
 };
